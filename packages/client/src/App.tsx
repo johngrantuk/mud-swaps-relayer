43fb1597d7f77a8a4a929cd71bb45a3ffec125b9
johngrantuk <johngrantuk@googlemail.com>
2023-05-21 20:30:55 +0100

    Added very rough Dino game.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useRows } from "@latticexyz/react";
 import { useMUD } from "./MUDContext";
+import Dino from "./dino/dino";
 
 export const App = () => {
   const {
@@ -8,21 +9,30 @@ export const App = () => {
     network: { storeCache },
   } = useMUD();
 
+  const cacti = [1];
+
+  const toggle = () => {
+    cacti.push(1);
+    console.log(`TEST`, cacti.length);
+  }
+
   const swaps = useRows(storeCache, { table: "Swap" });
   console.log(swaps.length);
   
   return (
     <>
-      {/* <button
+      {/* <Dino cacti={cacti}/> */}
+      <hr/>
+      <button
         type="button"
         onClick={async (event) => {
           event.preventDefault();
-          console.log("new counter value:", await increment());
+          toggle();
         }}
       >
-        Increment
-      </button> */}
-      <span>Swaps:</span>
+        Swap
+      </button>
+      <div>Swaps:</div>
       <ul>
         {swaps.map(({ value }, increment) => (
           <li key={increment}>
